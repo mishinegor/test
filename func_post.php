06ac0fc5b02d09fd5eafde38e136d7fb2b5b0f02
mishinegor <me@mishinegor.me>
2017-01-03 00:36:07 +0300

    Lesson 5 correct

--- func_post.php
+++ func_post.php
@@ -1,42 +1,41 @@
 <?php
 
 // Функция вывода всего списка новостей.
-function news_list($news) {
+function news_list($news)
+{
     // Сдвиг индекса на массива на 1
     array_unshift($news, 0);
     unset($news[0]);
-
     foreach ($news as $key => $val) { //Вывод новостей
-        if(isset($_POST['id'])){ // Проверка если новость существует
-            echo '<b>'."Новость ".$key.': </b>'.$val.'</br>';
-        }
+        echo '<b>' . "Новость " . $key . ': </b>' . $val . '</br>';
     }
 }
-
 
 
 
 // Функция вывода конкретной новости.
 function news_item($news)
 {
-    global $item;
+    static $item;
 
     // Сдвиг индекса на массива на 1
-    array_unshift($news, 0);
-    unset($news[0]);
+    if(isset($_POST['id'])) { // Проверка если новость существует)
+        array_unshift($news, 0);
+        unset($news[0]);
+        foreach ($news as $key => $val) { //Вывод новости
+            $key = $_POST['id']; // Индекс новости = id
+            if (isset($news[$key])) {
+                $item = '<b>' . "Новость " . $key . ': </b>' . $news[$key] . '</br>';
+            }
 
-    foreach ($news as $key => $val) { //Вывод новостей
-        $key = $_POST['id']; // Индекс новости = id
-        if (isset($news[$key])) {
-            $item = '<b>' . "Новость " . $key . ': </b>' . $news[$key] . '</br>';
-        } else {
-            header("HTTP/1.0 404 Not Found");
         }
-    }
-    if ($_POST['id'] < count($news) && isset($_POST['id'])) {
         echo $item;
+
+        if ($_POST['id'] > count($news)) {
+            news_list($news);
+        }
     } else {
-        news_list($news);
+        header("HTTP/1.0 404 Not Found");
     }
 }
 ?>
