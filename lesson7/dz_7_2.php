10da3aab173ef2594668c76f5f108024c59e62c3
mishinegor <me@mishinegor.me>
2017-01-24 23:07:30 +0300

    Lesson 7 file

--- lesson7/dz_7_2.php
+++ lesson7/dz_7_2.php
@@ -11,7 +11,7 @@
     $show_param = filter_var($_GET['show'], FILTER_SANITIZE_URL);
     $id = filter_var($_GET['id'], FILTER_SANITIZE_URL);
 
-    $id = $_GET['show'];
+     $id = $_GET['show'];
     $button_value="Добавить объявление";
 
     if(isset($_POST['add'])) { // Добавление записи
